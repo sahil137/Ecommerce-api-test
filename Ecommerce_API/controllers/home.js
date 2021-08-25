bb7d5fa6cced667d05ff07f237553d17f1dfc493
Sahil Saini <46193454+sahil137@users.noreply.github.com>
2021-08-26 01:46:39 +0530

    add a simple home route/controller

---
+++ Ecommerce_API/controllers/home.js
@@ -0,0 +1,5 @@
+export const home = (req, res) => {
+  res.send(
+    'This is a simple ecommerce api as an assignment/test for coding ninjas'
+  );
+};
--- Ecommerce_API/routes/index.js
+++ Ecommerce_API/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
 import productRoutes from './products.js';
-
+import homeRoute from './home.js';
 const router = express.Router();
 
+router.use('/', homeRoute);
 router.use('/products', productRoutes);
 
 export default router;
